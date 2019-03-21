39b97683c9373b70122c128302299a708afe4d75
xiaogouman <xiaogouman@hotmail.com>
2019-03-21 23:34:01 +0800

    save

--- build_sc.py
+++ build_sc.py
@@ -10,74 +10,212 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from torch.utils.data import Dataset
 
 import pickle
 
-class CNN(nn.Module):
-    def __init__(self):
-        super(CNN, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+EMBEDDING_DIM = 300
+INPUT_DIM = 1500
+KERNEL_SIZES = [3]
+epochs = 1
+running_loss = 0
+print_every = 10
+learning_rate = 0.001
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+
+# embedding in pytorch -> https://pytorch.org/tutorials/beginner/nlp/word_embeddings_tutorial.html
+# cnn in pytorch -> https://github.com/yunjey/pytorch-tutorial/blob/master/tutorials/02-intermediate/convolutional_neural_network/main.py#L35-L56
+# pretrained embedding in pytorch -> https://medium.com/@martinpella/how-to-use-pre-trained-word-embeddings-in-pytorch-71ca59249f76
+# text classification using keras -> https://realpython.com/python-keras-text-classification/#what-is-a-word-embedding
+# pytorch basics -> https://cs230-stanford.github.io/pytorch-getting-started.html
+
+class DatasetDocs(Dataset):
+
+    def __init__(self, X, Y, transform=None):
+        self.X = X
+        self.Y = Y
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, index):
+        # load image as ndarray type (Height * Width * Channels)
+        # be carefull for converting dtype to np.uint8 [Unsigned integer (0 to 255)]
+        # in this example, i don't use ToTensor() method of torchvision.transforms
+        # so you can convert numpy ndarray shape to tensor in PyTorch (H, W, C) --> (C, H, W)
+        doc = torch.Tensor(self.X[index])
+        label = torch.Tensor(self.Y[index])
+
+        if self.transform is not None:
+            doc = self.transform(doc)
+
+        return doc, label
+
+
+class ConvNet(nn.Module):
+    def create_embedding_layer(self, weight_matrix):
+        # layer = nn.Embedding(len(weight_matrix), len(weight_matrix[0]), padding_idx=0)
+        # weight = torch.FloatTensor([[1, 2.3, 3], [4, 5.1, 6.3]])
+        layer = nn.Embedding.from_pretrained(torch.FloatTensor(weight_matrix))
+
+        # weight = torch.FloatTensor([[1, 2.3, 3], [4, 5.1, 6.3]])
+        # layer = nn.Embedding.from_pretrained(weight)
+        return layer
+
+    def create_conv_layers(self, input_dim, kernal_sizes, n_filters=32):
+        layers = []
+        for kernal_size in kernal_sizes:
+            layer = nn.Conv1d(input_dim, n_filters, kernel_size=kernal_size, stride=1, padding=kernal_size-1)
+            layers.append(layer)
+            self.max_pooled_vev_dim = self.max_pooled_vev_dim + n_filters
+        return layers
+
+    def __init__(self, weight_matrix, kernal_sizes, num_classes=2):
+        super(ConvNet, self).__init__()
+        self.max_pooled_vev_dim = 0
+        self.embedding = nn.Embedding.from_pretrained(torch.FloatTensor(weight_matrix))
+        self.conv_layers = self.create_conv_layers(INPUT_DIM, kernal_sizes)
+        self.fc = nn.Linear(self.max_pooled_vev_dim, num_classes)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+        out = self.embedding(x)
+        cat_output = torch.Tensor()
+        for layer in self.conv_layers:
+            layer_output = layer(out)
+            layer_output = F.relu(layer_output)
+            layer_output = F.max_pool1d(torch.t(layer_output))
+            cat_output = torch.cat((cat_output, layer_output), 1)
 
-# input is file name
-# output is matrix: row is each text and
+        out = self.fc(cat_output)
+        out = F.softmax(out)
+        return out
+
+
+
+# input:  is file name
+# output: matrix that each row is [w1, w2, w3 ...] for each doc, and set of all vocabulary
 def load_train_docs(train_text_file):
     result = []
+    max_length = 0
+    target_vocab = set()
+    word_index = dict()
+    index = 1 # index = 0 is used for padding
     with open(train_text_file, "r") as file:
         for line in file:
-            result.append(line.split(" "))
-    return result
+            # TODO: tokenize words
+            words = line.split(" ")
+            result.append(words)
+            target_vocab.update(words)
+            if len(words) > max_length:
+                max_length = len(words)
+    print ("max number of words: ", max_length)
+    for word in target_vocab:
+        word_index[word] = index
+        index = index + 1
 
-def load_word_embeddings(embeddings_file):
-    if os.path.exists('embeddings.pickle'):
-        with open('embeddings.pickle', 'rb') as file:
-            embedding = pickle.load(file, encoding='utf-8')
-        return embedding
+    index_input = []
+    for doc in result:
+        idxs = [word_index[word] for word in doc]
+        index_input.append(idxs)
+    return index_input, word_index
+
+def load_word_embeddings(embeddings_file, word_index):
+    file_path = '../weight_matrix.npy'
+    if os.path.exists(file_path):
+        print('start loading weight_matrix')
+        weight_matrix = np.load(file_path)
+        print('finish loading weight_matrix')
+        return weight_matrix
+
+    print('start reading from embedding file')
     embedding = {}
+    weight_matrix = np.zeros((len(word_index), EMBEDDING_DIM))
     with gzip.open(embeddings_file, "rb") as file:
         for l in file:
             line = l.decode().encode('utf-8').decode('utf-8').split()
-            word = line[0]
-            vect = np.array(line[1:]).astype(np.float)
+            word, vect = line[0], np.array(line[1:]).astype(np.float)
             embedding[word] = vect
-    with open('embeddings.pickle', 'wb') as file:
-        pickle.dump(embedding, file)
-    return embedding
+    print('finish reading from embedding file')
+
+    # if pretrained matrix does not exist, generate random weight
+    found = 0
+    for word, idx in word_index.items():
+        if word in embedding:
+            found = found + 1
+            weight_matrix[idx] = embedding[word]
+        else:
+            weight_matrix[idx] = np.random.normal(scale=0.6, size=(EMBEDDING_DIM,))
+    print('total vocab: ', len(word_index), 'found: ', found)
+
+    print('start saving weight_matrix')
+    np.save(file_path, weight_matrix)
+    print('finish saving weight_matrix')
+    return weight_matrix
 
 def load_train_label(train_label_file):
     result = []
     with open(train_label_file, "r") as file:
         for line in file:
-            result.append(line)
+            result.append(int(line))
     return result
+
+from torch.utils import data
+
+def split_data(dataset):
+    n = len(dataset)
+    n_train = int(n*0.8)
+    n_val = n - n_train
+    train_set, val_set = data.random_split(dataset, (n_train, n_val))
+    return data.DataLoader(train_set), data.DataLoader(val_set)
+
+
+from torchsummary import summary
 
 def train_model(embeddings_file, train_text_file, train_label_file, model_file):
     # write your code here. You can add functions as well.
 	# use torch library to save model parameters, hyperparameters, etc. to model_file
 
     # load data
-    train_input = load_train_docs(train_text_file)
+    train_input, word_index = load_train_docs(train_text_file)
     train_label = load_train_label(train_label_file)
-    embeddings = load_word_embeddings(embeddings_file)
+    weight_matrix = load_word_embeddings(embeddings_file, word_index)
 
-    first2pairs = {k: embeddings[k] for k in list(embeddings)[:3]}
-    for key, value in first2pairs:
-        print(key, value)
+    model = ConvNet(weight_matrix,KERNEL_SIZES).to(device)
+    print(summary(model, (1000, 2000, 300)))
 
-    # how to use look up table??
+    train_dataset = DatasetDocs(train_input, train_label)
+    train_dataloader, val_dataloader = split_data(train_dataset)
+
+
+    ######## training ############
+    train_losses, test_losses = [], []
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    steps=0
+    total_steps=len(train_input)
+    for epoch in range(epochs):
+        for inputs, labels in train_dataloader:
+            steps += 1
+            inputs, labels = inputs.to(device), labels.to(device)
+
+            # forward pass
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+
+            # Backward and optimize
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            if (steps) % 100 == 0:
+                print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
+                      .format(epoch + 1, epochs, steps, total_steps, loss.item()))
+
+    torch.save(model.state_dict(), 'model.ckpt')
+
+
     print('Finished...')
 		
 if __name__ == "__main__":
