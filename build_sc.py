36e571547dd5341f992bf3012c48a4fb7ca49dd7
xiaogouman <xiaogouman@hotmail.com>
2019-03-25 23:47:46 +0800

    submit version

--- build_sc.py
+++ build_sc.py
@@ -22,17 +22,9 @@ LR = 0.001
 BATCH_SIZE = 50
 
 
-print(os.getcwd())
-file_path = '../weight_matrix.pkl'
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 print('device: ', device)
 
-
-# embedding in pytorch -> https://pytorch.org/tutorials/beginner/nlp/word_embeddings_tutorial.html
-# cnn in pytorch -> https://github.com/yunjey/pytorch-tutorial/blob/master/tutorials/02-intermediate/convolutional_neural_network/main.py#L35-L56
-# pretrained embedding in pytorch -> https://medium.com/@martinpella/how-to-use-pre-trained-word-embeddings-in-pytorch-71ca59249f76
-# text classification using keras -> https://realpython.com/python-keras-text-classification/#what-is-a-word-embedding
-# pytorch basics -> https://cs230-stanford.github.io/pytorch-getting-started.html
 
 class DatasetDocs(Dataset):
     def __init__(self, X, Y, transform=None):
@@ -65,9 +57,7 @@ class ConvNet(nn.Module):
         self.conv2 = nn.Conv1d(in_channels=EMBEDDING_DIM, out_channels=96, kernel_size=2, stride=1, padding=2-1)
         self.conv3 = nn.Conv1d(in_channels=EMBEDDING_DIM, out_channels=64, kernel_size=3, stride=1, padding=3-1)
         self.conv4 = nn.Conv1d(in_channels=EMBEDDING_DIM, out_channels=32, kernel_size=4, stride=1, padding=4-1)
-        self.conv5 = nn.Conv1d(in_channels=EMBEDDING_DIM, out_channels=16, kernel_size=5, stride=1, padding=5-1)
-        # self.conv5 = nn.Conv1d(in_channels=EMBEDDING_DIM, out_channels=N_FILTERS, kernel_size=5, stride=1, padding=5-1)
-        self.fc = nn.Linear(96+64+32+16, num_classes)
+        self.fc = nn.Linear(96+64+32, num_classes)
 
     def forward(self, x):
         out = self.embedding(x)
@@ -75,9 +65,7 @@ class ConvNet(nn.Module):
         out2 = F.relu(self.conv2(out)).max(dim=2)[0]
         out3 = F.relu(self.conv3(out)).max(dim=2)[0]
         out4 = F.relu(self.conv4(out)).max(dim=2)[0]
-        out5 = F.relu(self.conv5(out)).max(dim=2)[0]
-        out = torch.cat((out2, out3, out4, out5), dim=1)
-        # out = F.dropout(out, p=0.5)
+        out = torch.cat((out2, out3, out4), dim=1)
         out = self.fc(out)
         out = F.softmax(out, dim=1)
         return out
@@ -148,7 +136,6 @@ def load_word_embeddings(embeddings_file, word2idx):
     with gzip.open(embeddings_file, 'rt', encoding='utf-8') as f:
         embeddings = torch.rand(len(word2idx)+1, EMBEDDING_DIM) * 0.5 - 0.25
         embeddings[0] = torch.zeros((EMBEDDING_DIM,))
-        # embeddings = torch.zeros(len(word2idx)+1, EMBEDDING_DIM)
         for line in f:
             line = line.strip()
             first_space_pos = line.find(' ', 1)
@@ -173,13 +160,7 @@ def train_model(embeddings_file, train_text_file, train_label_file, model_file):
     train_input = word_to_idx_inputs(train_docs, word_index)
     train_dataset = DatasetDocs(train_input, train_label)
 
-    test_docs = load_docs('docs.test')
-    test_label = load_label('classes.test')
-    test_input = word_to_idx_inputs(test_docs, word_index)
-    test_dataset = DatasetDocs(test_input, test_label)
-
     train_dataloader = data.DataLoader(train_dataset, batch_size=BATCH_SIZE)
-    val_dataloader = data.DataLoader(test_dataset, batch_size=BATCH_SIZE)
 
     # define model
     embeddings = load_word_embeddings(embeddings_file, word_index)
@@ -208,15 +189,6 @@ def train_model(embeddings_file, train_text_file, train_label_file, model_file):
         # switch model to evaluation mode
         model.eval()
 
-        # calculate accuracy on validation set
-        n_val_correct, val_loss = 0, 0
-        with torch.no_grad():
-            for inputs, labels in val_dataloader:
-                outputs = model(inputs)
-                n_val_correct += (torch.max(outputs, 1)[1] == labels).sum().item()
-                val_loss = criterion(outputs, labels).item()
-        val_acc = n_val_correct / (len(val_dataloader) * BATCH_SIZE)
-
         n_train_correct, train_loss = 0, 0
         with torch.no_grad():
             for inputs, labels in train_dataloader:
@@ -225,8 +197,8 @@ def train_model(embeddings_file, train_text_file, train_label_file, model_file):
                 train_loss = criterion(outputs, labels).item()
         train_acc = n_train_correct / (len(train_dataloader) * BATCH_SIZE)
 
-        print('Epoch [{}/{}], Train acc: {:.4f}, Train Loss: {:.4f}, Val Acc: {:.4f}, Val Loss: {:.4f}'
-              .format(epoch + 1, EPOCHS, train_acc, train_loss, val_acc, val_loss))
+        print('Epoch [{}/{}], Train acc: {:.4f}, Train Loss: {:.4f}'
+              .format(epoch + 1, EPOCHS, train_acc, train_loss))
 
     torch.save(model, '{}/model.pth'.format(model_file))
     print('Finished...')
@@ -234,25 +206,11 @@ def train_model(embeddings_file, train_text_file, train_label_file, model_file):
 
 if __name__ == "__main__":
     # make no changes here
-    embeddings_file = sys.argv[1] if len(sys.argv) > 3 else "/Users/xiaogouman/Documents/masters/CS5246/Assignment/assignment_1/vectors.txt.gz"
-    train_text_file = sys.argv[2] if len(sys.argv) > 3 else "docs.train"
-    train_label_file = sys.argv[3] if len(sys.argv) > 3 else "classes.train"
-    model_file = sys.argv[4] if len(sys.argv) > 3 else "model_file"
+    embeddings_file = sys.argv[1]
+    train_text_file = sys.argv[2]
+    train_label_file = sys.argv[3]
+    model_file = sys.argv[4]
 
     if not os.path.exists(model_file):
         os.makedirs(model_file)
-    # train_model(embeddings_file, train_text_file, train_label_file, model_file)
-
-    from _datetime import datetime
-    for BATCH_SIZE in [100]:
-        for MAX_LENGTH in [500, 1000]:
-            for LR in [0.0005]:
-                print('=========== start of train ============')
-                print('BATCH_SIZE: ', BATCH_SIZE)
-                print('MAX_LENGTH: ', MAX_LENGTH)
-                print('LR: ', LR)
-                time_before = datetime.now()
-                train_model(embeddings_file, train_text_file, train_label_file, model_file)
-                time_taken = (datetime.now()-time_before).total_seconds() / 60
-                print('time taken: {:.2f}'.format(time_taken))
-                print('=========== end of train ============')
+    train_model(embeddings_file, train_text_file, train_label_file, model_file)
